240908aab09ff5a8d302ff19ca25f8acce7ffbc0
rasel <russelroy15@gmail.com>
2022-01-04 02:45:52 +0600

    new code added

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,10 +1,10 @@
-const numbers = [12, 44, 21, 54, 65, 78, 34, 97, 11, 97, 2, 92, 8, 23];
+const numbers = [52, 12, 44, 21, 54, 65, 78, 34, 97, 11, 90, 2, 92, 8, 23];
 
 function bubble_sort(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
       if (array[j] > array[j + 1]) {
-        [array[j], array[j + 1]] = [array[j + 1], array[y]];
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
