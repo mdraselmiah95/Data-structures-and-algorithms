75a4eb7ad4a99fcd3fb3acea43f358315c7fbb91
rasel <russelroy15@gmail.com>
2022-01-04 02:00:55 +0600

    new code added

---
+++ binary_search.js
@@ -0,0 +1,20 @@
+const numbers = [1, 5, 13, 25, 46, 78, 89, 90, 123, 345, 566, 999];
+
+function binary_search(array, target) {
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (array[mid] === target) {
+      return mid;
+    }
+    if (target < array[mid]) {
+      end = mid - 1;
+    }
+    if (target > array[mid]) {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
