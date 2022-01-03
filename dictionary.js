4d662041c6bbe0625a3a51e645662d9ac3f10f74
rasel <russelroy15@gmail.com>
2022-01-04 01:34:35 +0600

    new code added

--- dictionary.js
+++ dictionary.js
@@ -17,8 +17,13 @@ phoneBook.add("George Clooney", "548902356236");
 phoneBook.add("Johnny Depp", "52142834085");
 phoneBook.add("Ben Affleck", "6532168452");
 phoneBook.add("Kevin Spacey", "729374098");
+phoneBook.add("Robert De Niro", "623542367");
+phoneBook.add("Brad Pitt", "08193472897");
 
 console.log(phoneBook.dictionary);
 
-const Johnny = phoneBook.get("Johnny Depp");
-console.log(Johnny);
+// const Johnny = phoneBook.get("Johnny Depp");
+// console.log(Johnny);
+
+const Niro = phoneBook.get("Robert De Niro");
+console.log(Niro);
