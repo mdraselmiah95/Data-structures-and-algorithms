6bc23144c2dd7f3efc7782820e0950af9ca87e76
rasel <russelroy15@gmail.com>
2022-01-04 01:30:26 +0600

    new code added

--- dictionary.js
+++ dictionary.js
@@ -9,3 +9,13 @@ class Dictionary {
     return this.dictionary[key];
   }
 }
+
+const phoneBook = new Dictionary();
+
+phoneBook.add("Matt Damon", "012348927348");
+phoneBook.add("George Clooney", "548902356236");
+phoneBook.add("Johnny Depp", "52142834085");
+phoneBook.add("Ben Affleck", "6532168452");
+phoneBook.add("Kevin Spacey", "729374098");
+
+console.log(phoneBook.dictionary);
