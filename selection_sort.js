cbe58c67990376e73ce66718d64ad9d8e433fc2d
rasel <russelroy15@gmail.com>
2022-01-11 17:39:56 +0600

    new code added selection sort

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,14 @@
 const numbers = [45, 12, 5, 9, -5, 12];
+
+function selection_sort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    [array[i], array[min]] = [array[min], array[i]];
+  }
+  return array;
+}
